0c32ecf9b6c65a97eb1f60368739f3d91600d041
GodFatherTyrone <tygunnarbud@gmail.com>
2025-09-24 19:05:59 -0600

    prep work
    prep 3, 4, and 5 are done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,80 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        //asking user to make a list that stops on 0
+        List<int> data = new List<int>();
+        int input;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do
+        {
+            Console.Write("Enter number: ");
+            string guessS = Console.ReadLine();
+            input = int.Parse(guessS);
+            data.Add(input);
+
+            
+
+        } while (input != 0);
+        
+        //check to see if the numbers were recorded
+        /*
+        foreach (int number in data)
+        {
+            Console.WriteLine(number);
+        }
+        */
+
+        //this is to find the sum of the list "data"
+        int sum = 0;
+        foreach (int number in data)
+        {
+            sum = sum + number;
+        }
+        Console.WriteLine($"The sum of all the numbers in the data is: {sum}");
+
+        //this is to find the average in the list "data"
+        int i = 0;
+        float avg = 0;
+        foreach (int number in data)
+        {
+            i++;
+        }
+        avg = sum / i;
+        Console.WriteLine($"The average of the numbers in the data is: {avg}");
+
+        //this is to find the biggest in the list "data"
+        int biggest = 0;
+        foreach (int number in data)
+        {
+            if (number > biggest)
+            {
+                biggest = number;
+            }
+        }
+        Console.WriteLine($"The biggest number in the data is: {biggest}");
+        
+
+
+
+        /*
+        data.Add("phone");
+        data.Add("keyboard");
+        data.Add("mouse");
+
+        Console.WriteLine(data.Count);
+
+        
+
+        for (int i = 0; i < data.Count; i++)
+        {
+            Console.WriteLine(data[i]);
+        }
+        */
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,62 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    //I need to separate all things
+    public static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        
+        DisplayWelcome();
+
+        string name = PromptUserName();
+
+        int favnumber = PromptUserNumber();
+
+        int square = SquareNumber(favnumber);
+
+        int year;
+        PromtUserBirthYear(out year);
+
+        
+
+        DisplayResult(name, square, year);
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string uname = Console.ReadLine();
+        return uname;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favortite number: ");
+        string numberS = Console.ReadLine();
+        int favnumber = int.Parse(numberS);
+        return favnumber;
+    }
+
+    static void PromtUserBirthYear(out int year)
+    {
+        Console.Write("Please enter the year you were born: ");
+        string yearS = Console.ReadLine();
+        year = int.Parse(yearS);
+    }
+
+    static int SquareNumber(int favnumber)
+    {
+        int square = favnumber * favnumber;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square, int year)
+    {
+        Console.WriteLine($"{name}, the square of your favorite number is {square}");
+        Console.WriteLine($"{name}, you will turn {2025 - year} this year");
     }
 }
