9a62d936f4facb21ea0cff85b5f01f24ca108642
GodFatherTyrone <tygunnarbud@gmail.com>
2025-10-21 14:21:04 -0600

    word

---
+++ prove/Develop03/word.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel;
+
+public class word
+{
+    /*private string _word;*/
+    private bool _used;
+    private List<word> words = new List<word>();
+
+    private word()
+    {
+
+    }
+    private word(string word)
+    {
+        add._word //to list 
+        _used= false;
+    }
+}
